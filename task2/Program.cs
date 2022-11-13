b006459c360db78a1646240f220db91d57fd268d
Alex Ur <ru.booking@yandex.ru>
2022-11-13 22:12:40 +0300

    add fix in task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -10,26 +10,21 @@
 Console.WriteLine("Введите первое число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 
-
 Console.WriteLine("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
-int max=numberA;
-int min=numberB;
+int max=0;
+int min=0;
 
 if (numberA<numberB)
 {
     Console.WriteLine($"max={numberB}, min={numberA}");
 }
-// else
-// {
-//     Console.WriteLine($"max={numberA}, min={numberB}");
-// }
-// if (numberA<numberC)
-// {
-//     Console.WriteLine($"max={numberC}, min={numberA}");
-// }
-// else
-// {
-//     Console.WriteLine($"max={numberA}, min={numberC}");
-// }
+else if (numberA>numberB)
+{
+    Console.WriteLine($"max={numberA}, min={numberB}");
+}
+else if (max==min)
+{
+    Console.WriteLine("Эти числа равны.");
+}
